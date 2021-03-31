8726b4c5c534856b42e9d51319440a4ebb4bf108
Reflexa <65573831+Reflexa@users.noreply.github.com>
2021-03-31 11:59:44 +0300

    Create Working-Days.java

---
+++ Working-Days.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Working-Days {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+      System.out.print("Въведи ден = ");
+            String day = (scanner.nextLine());
+            switch (day) {
+                    case "Monday":
+                    case "Tuesday":
+                    case "Wednesday":
+                    case "Thursday":
+                    case "Friday":
+                    System.out.println("Working day");
+                    break;
+                    case "Saturday":
+                    case "Sunday":
+                    System.out.println("Weekend");
+                    break;
+                    default:
+                    System.out.println("Error");
+                    break;
+            }
+        }
+}
